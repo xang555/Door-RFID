2ffadb06b90999beb70e776bf9d72d5b48392172
xang <xang.ultimate@gmail.com>
2017-12-29 18:49:50 +0700

    fix web ui

--- Esp01/Esp01.ino
+++ Esp01/Esp01.ino
@@ -3,16 +3,30 @@
 #include <ESP8266WebServer.h>
 #include "data.h"
 
-#define bufSize 6000
-int bufc = 0; //buffer counter;
-
-char data_websiteBuffer[bufSize];
-
 /* Set these to your desired credentials. */
 const char *ssid = "Laoitdev-Door-RFID-01";
 const char *password = "12345678";
 
 ESP8266WebServer server(80);
+
+
+/* authentication */
+
+//Check if header is present and correct
+bool is_authentified(){
+  Serial.println("Enter is_authentified");
+  if (server.hasHeader("Cookie")){   
+    Serial.print("Found cookie: ");
+    String cookie = server.header("Cookie");
+    Serial.println(cookie);
+    if (cookie.indexOf(verify_cookie) != -1) {
+      Serial.println("Authentification Successful");
+      return true;
+    }
+  }
+  Serial.println("Authentification Failed");
+  return false;  
+}
 
 
 /* send response to client */
@@ -29,15 +43,37 @@ void sendFile(int code, String type, const char* adr, size_t len) {
 
 /* handle load ui page */
 void handleRoot() {
-	sendFile(200,"text/html",data_indexHTML,sizeof(data_indexHTML));
+   if (!is_authentified()){
+    String header = "HTTP/1.1 301 OK\r\nLocation: /login\r\nCache-Control: no-cache\r\n\r\n";
+    server.sendContent(header);
+    return;
+  }
+  sendFile(200,"text/html",data_indexHTML,sizeof(data_indexHTML)); 
 }
 
 void handleLogin(){
+
+  if (server.hasArg("uname") && server.hasArg("passwd")){
+    if (server.arg("uname") == Uname &&  server.arg("passwd") == Passwd ){
+      String header = "HTTP/1.1 301 OK\r\nSet-Cookie: "+verify_cookie+"\r\nLocation: /\r\nCache-Control: no-cache\r\n\r\n";
+      server.sendContent(header);
+      Serial.println("Log in Successful");
+      return;
+    }
+    String header = "HTTP/1.1 301 OK\r\nLocation: /error\r\nCache-Control: no-cache\r\n\r\n";
+    server.sendContent(header);
+    Serial.println("Log in Failed");
+    return;
+  }
   sendFile(200,"text/html",data_loginHTML,sizeof(data_loginHTML));
 }
 
 void handleErrorPage(){
     sendFile(200,"text/html",data_errorHTML,sizeof(data_errorHTML));
+}
+
+void handleSuccess() {
+   sendFile(200,"text/html",data_successHTML,sizeof(data_successHTML));
 }
 
 void handleLoadBootstrap(){
@@ -52,10 +88,29 @@ void handleLoadStyleCss(){
 	sendFile(200,"text/css;charset=UTF-8",data_styleCSS,sizeof(data_styleCSS));
 }
 
+void handleSendCommand() {
+
+  if (!is_authentified()){
+    String header = "HTTP/1.1 301 OK\r\nLocation: /login\r\nCache-Control: no-cache\r\n\r\n";
+    server.sendContent(header);
+    return;
+  }
+  
+  if(server.hasArg("cmd") && server.hasArg("submit")) {
+    if(server.arg("cmd") == "1") {
+        Serial.write("open");
+        String header = "HTTP/1.1 301 OK\r\nLocation: /success\r\nCache-Control: no-cache\r\n\r\n";
+        server.sendContent(header);
+        return; 
+    }
+  }
+
+   String header = "HTTP/1.1 301 OK\r\nLocation: /error\r\nCache-Control: no-cache\r\n\r\n";
+   server.sendContent(header);
+  
+}
+
 /* handle API */
-
-
-
 
 
 void setup() {
@@ -70,11 +125,22 @@ void setup() {
 	Serial.print("AP IP address: ");
 	Serial.println(myIP);
 	server.on("/", handleRoot);
-    server.on("/login",handleLogin);
-    server.on("/error",handleErrorPage);
+  server.on("/login",handleLogin);
+  server.on("/error",handleErrorPage);
+  server.on("/success",handleSuccess);
+  server.on("/cmd",HTTP_POST,handleSendCommand);
+  server.onNotFound(handleErrorPage);
+  
 	server.on("/css/bootstrap.min.css",handleLoadBootstrap);
 	server.on("/css/bootstrap-theme.min.css",handleLoadBootStrapTheme);
 	server.on("/style.css",handleLoadStyleCss);
+
+  //here the list of headers to be recorded
+  const char * headerkeys[] = {"User-Agent","Cookie"} ;
+  size_t headerkeyssize = sizeof(headerkeys)/sizeof(char*);
+  //ask server to track these headers
+  server.collectHeaders(headerkeys, headerkeyssize );
+ 
 	server.begin();
 	Serial.println("HTTP server started");
 }
